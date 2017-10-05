071041b1ead3a61a0edab5179da633189be56411
Erling <erling.stage@elektrobit.com>
2017-10-05 17:31:30 +0300

    checkpoint

--- scripts/custom_talker.py
+++ scripts/custom_talker.py
@@ -1,12 +1,67 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import rospy
 from beginner_tutorials.msg import Wheels
+from beginner_tutorials.msg import Distance
+import serial
+import select
+import sys
+import termios
+import atexit
+import sensorhub
+
+pub = rospy.Publisher('custom_chatter', Wheels)
+
+wheelPublisher = Wheels()
+distancePublisher = Distance()
+
+def publishPing(ping):
+	return
+	
+def publishPong(ping):
+	return
+	
+def publishWheels(left, right):
+	global pub
+	global wheelPublisher
+
+	wheelPublisher.left.when = left.when
+	wheelPublisher.right.when = right.when
+
+	wheelPublisher.left.speed = left.speed
+	wheelPublisher.right.speed = right.speed
+	
+	wheelPublisher.left.direction = left.direction
+	wheelPublisher.right.direction = right.direction
+	
+	wheelPublisher.left.dist = left.turn
+	wheelPublisher.right.dist = right.turn
+	
+	wheelPublisher.left.dist_abs = right_dist
+	wheelPublisher.right.dist_abs = right.dist
+
+    rospy.loginfo(wheelPublisher)
+    pub.publish(wheelPublisher)
+	
+	return
+	
+def publishDist(dist):
+	
+	return
+
+def publishError(error):
+	return
 
 
 def talker():
-    pub = rospy.Publisher('custom_chatter', Wheels)
+	global pub
     rospy.init_node('custom_talker', anonymous=True)
     r = rospy.Rate(10) #10hz
+    
+    sh = sensorhub.Sensorhub()
+    sh.setOutputHandlers(publishPing, publishPong, publishWheels, publishDist, publishError)
+    
     msg = Wheels()
     msg.left.speed = 17
     msg.left.direction = 0
